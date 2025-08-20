76ecf42662b83139bcc41c0327d5d18357bc57c0
rishabhsabnavis <rishabhsabnavis@gmail.com>
2025-08-20 14:09:09 -0500

    testing code

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 from caveclient import CAVEclient
 import os
+from fafbseg import flywire
+import navis
 
 
+
+
+#Setup Cave
 client = CAVEclient("flywire_fafb_public")
 client.auth.save_token(token='b647de7b3ecda31cdb9c04e538de1f20', overwrite=True)
 
 
 
-root_id = '720575940626979621'
+
+
+#Put in desired root_id here!
+root_id = '720575940625102224'
+
+
 
 root_info = client.chunkedgraph.get_root_id(root_id)
 print("Root info:", root_info)
 
 
 #Get Children
+#children = client.chunkedgraph.get_children(root_id)
+#print("Children:", children)
 
-children = client.chunkedgraph.get_children(root_id)
 
-print("Children:", children)
 
+#setup ff
+flywire.set_default_dataset("public")
+
+
+#Mesh Neuron and Plotting
+meshy = flywire.get_mesh_neuron(root_id)
+
+navis.plot3d(meshy, color='red', soma=True, connectors=True)
 
 
 

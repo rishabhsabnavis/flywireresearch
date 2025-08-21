fb8131ff475466a5b66d5df4eefe1bb5f3a9446d
rishabhsabnavis <rishabhsabnavis@gmail.com>
2025-08-21 11:33:48 -0500

    Add FlyWire connectome analysis project files and .gitignore

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from caveclient import CAVEclient
 import os
 from fafbseg import flywire
 import navis
 
 
 
-
-#Setup Cave
-client = CAVEclient("flywire_fafb_public")
-client.auth.save_token(token='b647de7b3ecda31cdb9c04e538de1f20', overwrite=True)
 
 
 
@@ -19,13 +14,8 @@ root_id = '720575940625102224'
 
 
 
-root_info = client.chunkedgraph.get_root_id(root_id)
-print("Root info:", root_info)
 
 
-#Get Children
-#children = client.chunkedgraph.get_children(root_id)
-#print("Children:", children)
 
 
 
